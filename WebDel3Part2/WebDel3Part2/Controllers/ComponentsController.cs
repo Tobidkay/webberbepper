b3fb5fb76b9f6d1556a6cdc9d58e5b4d09eae8a9
Mikkel Overgaard Bitsch <201504472@post.au.dk>
2018-11-28 17:21:50 +0100

    Welcome to world of identity

--- WebDel3Part2/WebDel3Part2/Controllers/ComponentsController.cs
+++ WebDel3Part2/WebDel3Part2/Controllers/ComponentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using WebDel3Part2.Models;
 
 namespace WebDel3Part2.Controllers
 {
+    [Authorize]
     public class ComponentsController : Controller
     {
         private readonly ApplicationDbContext _context;
