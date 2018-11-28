48f436666eff7db6e7ad347883ffb52f3bc6ca11
Mikkel Overgaard Bitsch <201504472@post.au.dk>
2018-11-28 17:53:48 +0100

    Revert "Welcome to world of identity"
    This reverts commit b3fb5fb76b9f6d1556a6cdc9d58e5b4d09eae8a9.

--- WebDel3Part2/WebDel3Part2/Controllers/CategoriesController.cs
+++ WebDel3Part2/WebDel3Part2/Controllers/CategoriesController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +10,6 @@ using WebDel3Part2.Models;
 
 namespace WebDel3Part2.Controllers
 {
-    [Authorize]
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
